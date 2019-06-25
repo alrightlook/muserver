9b1c2165e903ea6b7277f5aa359fdfe60e8727f7
brian.kernighan <victor.chicu@hotmail.com>
2019-06-25 08:53:08 +0300

    connectserver: output messages just once.

--- connectserver/src/main/java/UdpConnectServerHandler.java
+++ connectserver/src/main/java/UdpConnectServerHandler.java
@@ -10,7 +10,6 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import settings.ConnectServerSettings;
 import settings.GameServerSettings;
-import tasks.HealthTask;
 
 import java.io.ByteArrayInputStream;
 import java.net.InetSocketAddress;
@@ -19,22 +18,63 @@ import java.util.concurrent.ConcurrentHashMap;
 import java.util.stream.Collectors;
 
 public class UdpConnectServerHandler extends SimpleChannelInboundHandler<DatagramPacket> {
+ private final static int DELAY_IN_MILLIS = 0;
+ private final static int PERIOD_IN_MILLIS = 1000;
+ private final static int PACKET_TIMEOUT_IN_MILLIS = 1000 * 5;
+ private final static Timer scheduler = new Timer();
  private final static Logger logger = LogManager.getLogger(UdpConnectServerHandler.class);
  private final Map<Short, GameServerSettings> gameServersSettingsMap = new HashMap<>();
  private final ConcurrentHashMap<Byte, AbstractPacket> packets = new ConcurrentHashMap<>();
 
  public UdpConnectServerHandler(ConnectServerSettings connectServerSettings) {
-  Map<Short, List<GameServerSettings>> gameServersGroupingBy = connectServerSettings.gameServers()
-      .stream()
-      .collect(Collectors.groupingBy(x -> x.serverCode()));
-
+  Map<Short, List<GameServerSettings>> gameServersGroupingBy = connectServerSettings.gameServers().stream().collect(Collectors.groupingBy(x -> x.serverCode()));
   for (Map.Entry<Short, List<GameServerSettings>> entry : gameServersGroupingBy.entrySet()) {
    gameServersSettingsMap.put(entry.getKey(), entry.getValue().get(0));
   }
+ }
 
-  Timer timer = new Timer();
+ @Override
+ public void channelActive(ChannelHandlerContext ctx) throws Exception {
+  scheduler.schedule(new TimerTask() {
+   @Override
+   public void run() {
+    for (Byte headCode : packets.keySet()) {
+     switch (headCode) {
+      case 1: {
+       AbstractPacket abstractPacket = packets.getOrDefault(headCode, null);
+       if (abstractPacket != null) {
+        if (new Date().getTime() - abstractPacket.packetTime().getTime() > PACKET_TIMEOUT_IN_MILLIS) {
+         logger.warn("Connection to the Game server has been interrupted");
+        }
+       }
+      }
+      break;
+      case 2: {
+       AbstractPacket abstractPacket = packets.getOrDefault(headCode, null);
+       if (abstractPacket != null) {
+        if (new Date().getTime() - abstractPacket.packetTime().getTime() > PACKET_TIMEOUT_IN_MILLIS) {
+         logger.warn("Connection to the Join server has been interrupted");
+        }
+       }
+      }
+      break;
+     }
+    }
+   }
+  }, DELAY_IN_MILLIS, PERIOD_IN_MILLIS);
+ }
 
-  timer.schedule(new HealthTask(packets), 0, 1000);
+ @Override
+ public void channelInactive(ChannelHandlerContext ctx) throws Exception {
+  packets.clear();
+  scheduler.cancel();
+  scheduler.purge();
+ }
+
+ @Override
+ public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+  logger.error(cause.getMessage(), cause);
+  ctx.close();
  }
 
  @Override
@@ -64,12 +104,18 @@ public class UdpConnectServerHandler extends SimpleChannelInboundHandler<Datagra
        throw new UdpConnectServerHandlerException(String.format("Server code %d mismatching configuration", gameServerInfo.serverCode()));
       }
 
+      if (!packets.containsKey(headCode)) {
+       logger.info(String.format("Connection to the game server %d has been established", gameServerInfo.serverCode()));
+      }
+
       packets.put(headCode, gameServerInfo);
      }
      break;
      case 2: {
       PMSG_JOINSERVERINFO joinServerInfo = PMSG_JOINSERVERINFO.deserialize(new ByteArrayInputStream(buffer));
-
+      if (!packets.containsKey(headCode)) {
+       logger.info("Connection to the join server has been established");
+      }
       packets.put(headCode, joinServerInfo);
      }
      break;
@@ -89,11 +135,5 @@ public class UdpConnectServerHandler extends SimpleChannelInboundHandler<Datagra
     }
    }
   }
- }
-
- @Override
- public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
-  logger.error(cause.getMessage(), cause);
-  ctx.close();
  }
 }
