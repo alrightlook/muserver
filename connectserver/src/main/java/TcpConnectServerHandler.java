42a84ad639e071430b62e5e606d35dfe3013bb06
Victor Chicu <victor.chicu@hotmail.com>
2019-06-26 18:37:41 +0300

    connectserver: Handle CS protocol (WIP)

--- connectserver/src/main/java/TcpConnectServerHandler.java
+++ connectserver/src/main/java/TcpConnectServerHandler.java
@@ -1,10 +1,14 @@
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelId;
 import io.netty.channel.SimpleChannelInboundHandler;
+import messages.PMSG_HEAD;
+import messages.PWMSG_HEAD;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import settings.ConnectServerSettings;
 
+import java.io.ByteArrayInputStream;
+import java.net.InetSocketAddress;
 import java.util.concurrent.ConcurrentHashMap;
 
 public class TcpConnectServerHandler extends SimpleChannelInboundHandler<byte[]> {
@@ -36,7 +40,58 @@ public class TcpConnectServerHandler extends SimpleChannelInboundHandler<byte[]>
  }
 
  @Override
- protected void channelRead0(ChannelHandlerContext channelHandlerContext, byte[] bytes) throws Exception {
-  logger.info(bytes);
+ protected void channelRead0(ChannelHandlerContext ctx, byte[] buffer) throws Exception {
+  if (buffer.length > 0) {
+   switch (buffer[0]) {
+    case (byte) 0xC1: {
+     PMSG_HEAD header = PMSG_HEAD.deserialize(new ByteArrayInputStream(buffer));
+     handleProtocol(header.headCode(), buffer[3]);
+    }
+    break;
+    case (byte) 0xC2: {
+     PWMSG_HEAD header = PWMSG_HEAD.deserialize(new ByteArrayInputStream(buffer));
+     handleProtocol(header.headCode(), buffer[4]);
+    }
+    break;
+   }
+  } else {
+   closeImmediately(ctx);
+  }
+ }
+
+ private void handleProtocol(byte headCode, byte subCode) {
+  switch (headCode) {
+   case 5: {
+    //todo:
+   }
+   break;
+
+   case 6: {
+    //todo:
+   }
+   break;
+
+   case (byte) 0xF4: {
+    switch (subCode) {
+     case 6: {
+      //todo:
+     }
+     break;
+     case 7: {
+      //todo:
+     }
+     break;
+    }
+   }
+   break;
+  }
+ }
+
+ private void closeImmediately(ChannelHandlerContext ctx) {
+  InetSocketAddress remoteAddress = (InetSocketAddress) ctx.channel().remoteAddress();
+  if (remoteAddress != null) {
+   logger.warn(String.format("Hacking attempt from: %s", remoteAddress.getAddress().getHostName()));
+   ctx.close();
+  }
  }
 }
