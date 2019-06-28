e2b68df7b0579a5d80f035aef28e53b70835a408
brian.kernighan <victor.chicu@hotmail.com>
2019-06-28 23:11:41 +0300

    connecrserver: Added 2 tests for server info & server list (WIP)

--- connectserver/src/test/java/TcpConnectServerHandlerTest.java
+++ connectserver/src/test/java/TcpConnectServerHandlerTest.java
@@ -1,36 +1,46 @@
 import enums.ServerType;
+import io.netty.buffer.Unpooled;
 import io.netty.channel.embedded.EmbeddedChannel;
-import messages.PMSG_GAMESERVERINFO;
-import messages.PMSG_HEAD;
 import org.junit.jupiter.api.Test;
 import settings.ConnectServerSettings;
 import settings.GameServerSettings;
 import settings.ListeningPortsSettings;
 
-import java.io.ByteArrayOutputStream;
 import java.util.Arrays;
 
 public class TcpConnectServerHandlerTest {
  private final static int GS_PORT = 55901, TCP_PORT = 44405, UDP_PORT = 55557;
+ private final static String HOSTNAME = "localhost";
 
  @Test
- public void testTcpConnectServerHandlerValidPacket() throws Exception {
+ public void testTcpConnectServerHandlerServerInfo() throws Exception {
   EmbeddedChannel embeddedChannel = new EmbeddedChannel(new TcpConnectServerHandler(
       ConnectServerSettings.create(
           ListeningPortsSettings.create(TCP_PORT, UDP_PORT),
           Arrays.asList(
               GameServerSettings.create(
-                  (short) 19, "GameServer", "localhost", GS_PORT, ServerType.VISIBLE
+                  (short) 0, "GameServer", HOSTNAME, GS_PORT, ServerType.VISIBLE
               )
           )
       )
   ));
 
-  PMSG_GAMESERVERINFO gsInfo = PMSG_GAMESERVERINFO.create(
-      PMSG_HEAD.create((byte) 0xC1, (byte) 10, (byte) 1),
-      (short) 19, (byte) 0, (short) 0, (short) 0, (short) 0, (short) 100
-  );
+  embeddedChannel.writeInbound(Unpooled.wrappedBuffer(new byte[0]));
+ }
 
-  embeddedChannel.writeInbound(gsInfo.serialize(new ByteArrayOutputStream()));
+ @Test
+ public void testTcpConnectServerHandlerServerList() throws Exception {
+  EmbeddedChannel embeddedChannel = new EmbeddedChannel(new TcpConnectServerHandler(
+      ConnectServerSettings.create(
+          ListeningPortsSettings.create(TCP_PORT, UDP_PORT),
+          Arrays.asList(
+              GameServerSettings.create(
+                  (short) 0, "GameServer", HOSTNAME, GS_PORT, ServerType.VISIBLE
+              )
+          )
+      )
+  ));
+
+  embeddedChannel.writeInbound(Unpooled.wrappedBuffer(new byte[0]));
  }
 }
