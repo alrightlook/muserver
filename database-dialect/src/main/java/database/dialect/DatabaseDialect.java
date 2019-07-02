61ccd3aae75e1c92d16a5f7d807d715d0bf114da
brian.kernighan <victor.chicu@hotmail.com>
2019-07-03 00:43:34 +0300

    database-dialect: Added SqlServerDatabaseDialect impl. (WIP)
    Added database connection provider impl.

--- database-dialect/src/main/java/database/dialect/DatabaseDialect.java
+++ database-dialect/src/main/java/database/dialect/DatabaseDialect.java
@@ -4,6 +4,6 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
-public interface DatabaseDialect extends ConnectionProvider {
+public interface DatabaseDialect extends DatabaseConnectionProvider {
  PreparedStatement createPreparedStatement(Connection connection, String query) throws SQLException;
 }
--- database-dialect/src/main/java/database/dialect/SqlServerDatabaseDialect.java
+++ database-dialect/src/main/java/database/dialect/SqlServerDatabaseDialect.java
@@ -1,56 +1,70 @@
 package database.dialect;
 
-import sun.security.util.Password;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
+import java.sql.*;
 import java.util.Properties;
 import java.util.Queue;
 import java.util.concurrent.ConcurrentLinkedQueue;
 
 public class SqlServerDatabaseDialect implements DatabaseDialect {
+ private final static Logger logger = LogManager.getLogger(SqlServerDatabaseDialect.class);
+ private final String hostname, instanceName, databaseName, username, password;
+ private final Integer portNumber;
  private final Queue<Connection> connections = new ConcurrentLinkedQueue<>();
- private final SqlServerConnection sqlServerConnection;
 
- public SqlServerDatabaseDialect(SqlServerConnection sqlServerConnection) {
-  this.sqlServerConnection = sqlServerConnection;
+ public SqlServerDatabaseDialect(String hostname, Integer portNumber, String instanceName, String databaseName, String username, String password) {
+  this.hostname = hostname;
+  this.portNumber = portNumber;
+  this.instanceName = instanceName;
+  this.databaseName = databaseName;
+  this.username = username;
+  this.password = password;
+ }
+
+ @Override
+ public PreparedStatement createPreparedStatement(Connection connection, String query) throws SQLException {
+  logger.trace("Creating a PreparedStatement '{}'", query);
+  PreparedStatement statement = connection.prepareStatement(query);
+  return statement;
  }
 
  @Override
  public void close() {
-
+  Connection connection;
+  while ((connection = connections.poll()) != null) {
+   try {
+    connection.close();
+   } catch (Throwable e) {
+    logger.warn("Error while closing connection", e);
+   }
+  }
  }
 
  @Override
  public boolean testConnection(Connection connection, int timeout) throws SQLException {
-  return false;
+  return connection.isValid(timeout);
  }
 
  @Override
  public Connection getConnection() throws SQLException {
   Properties properties = new Properties();
 
-  if (sqlServerConnection.username() != null) {
-   properties.setProperty("user", sqlServerConnection.username());
+  if (username != null) {
+   properties.setProperty("username", username);
   }
 
-  if (sqlServerConnection.password() != null) {
-   properties.setProperty("password", sqlServerConnection.password());
+  if (password != null) {
+   properties.setProperty("password", password);
   }
 
-  String sqlServerConnectionString = "jdbc:jtds:sqlserver://127.0.0.1;instance=MSSQLSERVER;DatabaseName=MuOnline";
+  String connectionString = String.format("jdbc:sqlserver://%s:%d;instanceName=%s;databaseName=%s;username=%s;password=%s", hostname, portNumber, instanceName, databaseName, username, password);
 
-  Connection connection = DriverManager.getConnection(sqlServerConnectionString, properties);
+  Connection connection = DriverManager.getConnection(connectionString, properties);
 
   connections.add(connection);
 
   return connection;
- }
-
- @Override
- public PreparedStatement createPreparedStatement(Connection connection, String query) throws SQLException {
-  return null;
  }
 }
