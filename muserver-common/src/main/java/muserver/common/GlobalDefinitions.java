2e3bc72abcd823e517a0452bd2f12abfa73790fa
Victor Chicu <victor.chicu@hotmail.com>
2019-07-04 18:17:57 +0300

    muserver-common: Added constants.

---
+++ muserver-common/src/main/java/muserver/common/GlobalDefinitions.java
@@ -0,0 +1,12 @@
+package muserver.common;
+
+public class GlobalDefinitions {
+    public final static int MAX_MAGIC = 20;
+    public final static int MAX_DBMAGIC = MAX_MAGIC * 3;
+    public final static int MAX_IDSTRING = 10;
+    public final static int MAX_GUILDNOTICE	= 60;
+    public final static int MAX_DBINVENTORY = 532;
+    public final static int MAX_GUILDNAMESTRING = 8;
+    public final static int MAX_WAREHOUSEDBSIZE = 840;
+
+}
