77544c514d58fdf128f25d57b4fa8a66eca4369f
brian.kernighan <victor.chicu@hotmail.com>
2019-07-06 00:58:52 +0300

    muserver-common: Added new constants.

--- muserver-common/src/main/java/muserver/common/Globals.java
+++ muserver-common/src/main/java/muserver/common/Globals.java
@@ -1,11 +1,18 @@
 package muserver.common;
 
 public class Globals {
-    public final static int MAX_MAGIC = 20;
-    public final static int MAX_DBMAGIC = MAX_MAGIC * 3;
-    public final static int MAX_IDSTRING = 10;
-    public final static int MAX_GUILDNOTICE	= 60;
-    public final static int MAX_DBINVENTORY = 532;
-    public final static int MAX_GUILDNAMESTRING = 8;
-    public final static int MAX_WAREHOUSEDBSIZE = 840;
+ public static final int MAX_CHAT = 60;
+ public static final int MAX_MAGIC = 20;
+ public static final int MAX_DBMAGIC = MAX_MAGIC * 3;
+ public static final int MAX_IDSTRING = 10;
+ public static final int MAX_ITEMNAME = 21;
+ public static final int MAX_ANSWERSTR = 30;
+ public static final int MAX_IPADDRESS = 17;
+ public static final int MAX_SERVERNAME = 20;
+ public static final int MAX_QUESTIONSTR = 30;
+ public static final int MAX_GUILDNOTICE = 60;
+ public static final int MAX_DBINVENTORY = 532;
+ public static final int MAX_JOOMINNUMBERSTR = 13;
+ public static final int MAX_GUILDNAMESTRING = 8;
+ public static final int MAX_WAREHOUSEDBSIZE = 840;
 }
