07dbccd9b1cd9c4e991f962c8c04298070516567
brian.kernighan <victor.chicu@hotmail.com>
2019-07-05 08:53:15 +0300

    muserver: minor name changes.

--- muserver-common/src/main/java/muserver/common/Globals.java
+++ muserver-common/src/main/java/muserver/common/Globals.java
@@ -1,6 +1,6 @@
 package muserver.common;
 
-public class GlobalDefinitions {
+public class Globals {
     public final static int MAX_MAGIC = 20;
     public final static int MAX_DBMAGIC = MAX_MAGIC * 3;
     public final static int MAX_IDSTRING = 10;
@@ -8,5 +8,4 @@ public class GlobalDefinitions {
     public final static int MAX_DBINVENTORY = 532;
     public final static int MAX_GUILDNAMESTRING = 8;
     public final static int MAX_WAREHOUSEDBSIZE = 840;
-
 }
