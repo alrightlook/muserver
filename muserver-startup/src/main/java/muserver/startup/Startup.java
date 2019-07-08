f24b43940fad7815304d7ccf681ad54021e86537
brian.kernighan <victor.chicu@hotmail.com>
2019-07-08 22:59:18 +0300

    muserver-startup: minor changes.

--- muserver-startup/src/main/java/muserver/startup/Startup.java
+++ muserver-startup/src/main/java/muserver/startup/Startup.java
@@ -11,44 +11,44 @@ import org.apache.logging.log4j.Logger;
 import java.util.concurrent.CompletableFuture;
 
 public class Startup {
-    private final static Logger logger = LogManager.getLogger(Startup.class);
+ private final static Logger logger = LogManager.getLogger(Startup.class);
 
-    public static void main(String[] args) throws Exception {
-        String path = "./startup.json";
+ public static void main(String[] args) throws Exception {
+  String path = "./startup.json";
 
-        IServer joinServer = new JoinServer(path), connectServer = new ConnectServer(path);
+  IServer joinServer = new JoinServer(path), connectServer = new ConnectServer(path);
 
-        CompletableFuture<Void> startupFeature = CompletableFuture.runAsync(() -> {
-            try {
-                connectServer.startup();
-            } catch (ServerException e) {
-                logger.error(e.getMessage(), e);
-            }
-        }).thenRun(() -> {
-            try {
-                joinServer.startup();
-            } catch (ServerException e) {
-                logger.error(e.getMessage(), e);
-            }
-        }).exceptionally(throwable -> {
-            logger.error(throwable.getMessage(), throwable);
-            return null;
-        });
+  CompletableFuture<Void> startupFeature = CompletableFuture.runAsync(() -> {
+   try {
+    connectServer.startup();
+   } catch (ServerException e) {
+    logger.error(e.getMessage(), e);
+   }
+  }).thenRun(() -> {
+   try {
+    joinServer.startup();
+   } catch (ServerException e) {
+    logger.error(e.getMessage(), e);
+   }
+  }).exceptionally(throwable -> {
+   logger.error(throwable.getMessage(), throwable);
+   return null;
+  });
 
-        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
-            try {
-                if (joinServer != null) {
-                    joinServer.shutdown();
-                }
-
-                if (connectServer != null) {
-                    connectServer.shutdown();
-                }
-            } catch (Exception e) {
-                logger.error(e.getMessage(), e);
-            }
-        }));
-
-        startupFeature.get();
+  Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+   try {
+    if (joinServer != null) {
+     joinServer.shutdown();
     }
+
+    if (connectServer != null) {
+     connectServer.shutdown();
+    }
+   } catch (Exception e) {
+    logger.error(e.getMessage(), e);
+   }
+  }));
+
+  startupFeature.get();
+ }
 }
