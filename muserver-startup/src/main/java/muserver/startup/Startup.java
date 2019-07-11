2b09df29ca9cd4e1d4a2869c3399b7799915e992
Victor Chicu <victor.chicu@hotmail.com>
2019-07-11 11:20:42 +0200

    muserver-startup: Added cli.

--- muserver-startup/src/main/java/muserver/startup/Startup.java
+++ muserver-startup/src/main/java/muserver/startup/Startup.java
@@ -5,6 +5,11 @@ import muserver.common.exceptions.ServerException;
 import muserver.connectserver.ConnectServer;
 import muserver.connectserver.exceptions.ConnectServerException;
 import muserver.joinserver.JoinServer;
+import muserver.joinserver.exceptions.JoinServerException;
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.DefaultParser;
+import org.apache.commons.cli.Options;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -14,8 +19,20 @@ public class Startup {
  private final static Logger logger = LogManager.getLogger(Startup.class);
 
  public static void main(String[] args) throws Exception {
-  String path = "./startup.json";
+  CommandLineParser parser = new DefaultParser();
 
+  Options cliOptions = new Options();
+
+  cliOptions.addOption("p", "path", true, "path to configuration file");
+
+  CommandLine cl = parser.parse(cliOptions, args);
+
+  if (!cl.hasOption("p")) {
+   throw new JoinServerException("Arg --path is required");
+  }
+
+  String path = cl.getOptionValue("p");
+  
   IServer joinServer = new JoinServer(path), connectServer = new ConnectServer(path);
 
   Runtime.getRuntime().addShutdownHook(new Thread(() -> {
